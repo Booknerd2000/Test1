288d23bd9d324b65d8c5be5e907e8a66370db62e
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-04 07:50:48 -0500

    Added Me myself and I

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,7 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Me, myself");
+	System.out.println("and I");
     }
 }
