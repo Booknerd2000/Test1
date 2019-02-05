9fcb6ae3c1f01fc0d9da6457c65bff976746e222
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-05 08:02:56 -0500

    summit changes from home computer

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	System.out.println("Me, myself");
 	System.out.println("and I");
 	System.out.println("solo fight");
-	System.out.println("until I");
+	System.out.println("i forgot the rest of the song");
     }
 }
